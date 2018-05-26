2a78f0c12a19416da946a84f0074d4fb55b13e07
tanuj1998 <tyadav@purdue.edu>
2018-05-26 21:20:34 +0530

    Prep repo for heroku

--- public/bundle.js
+++ public/bundle.js
@@ -26672,9 +26672,18 @@
 
 	  render: function render() {
 	    return React.createElement(
-	      'h3',
+	      'div',
 	      null,
-	      'Examples Component'
+	      React.createElement(
+	        'h3',
+	        null,
+	        'Examples Component!'
+	      ),
+	      React.createElement(
+	        'p',
+	        null,
+	        'Welcome to example page!'
+	      )
 	    );
 	  }
 	});
